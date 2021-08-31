196fcc6cdc88e92187f836a4502fdbedb18666a1
xTeKc <bcd3v@pm.me>
2021-08-31 11:18:18 -0400

    setup main func

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,12 @@ use qrcode::QrCode;
 use image::Luma;
 
 fn main() {
-    println!("Hello, world!");
+    // Encode some data into bits.
+    let code = QrCode::new("This is the QR code message").unwrap();
+
+    // Render the bits into an image.
+    let image = code.render::<Luma<u8>>().build();
+
+    // Save the image.
+    image.save("img/qrcode.png").unwrap();
 }
