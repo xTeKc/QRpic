c1b599e1fbe655469cc44e360d7a744d45ed799e
xTeKc <bcd3v@pm.me>
2021-09-01 22:55:39 -0400

    update

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 use qrcode::QrCode;
 use image::Luma;
 
-fn main() {
-    // Encode some data into bits.
-    let code = QrCode::new("This is the QR code message").unwrap();
+#[macro_use]
+extern crate fstrings;
 
+fn output() {
+    let mut line = String::new();
+    println!("Enter Output: \n");
+    let b1 = std::io::stdin().read_line(&mut line).unwrap();
+    println!("\nHello, here is your output: {}", line);
+    println!("Number of Bytes Read: {}", b1);
+}
+
+fn main() {
+    let output_data = output();
+
+    // Encode some data into bits.
+    let code = QrCode::new(" ").unwrap();
+
+    
+    
     // Render the bits into an image.
     let image = code.render::<Luma<u8>>().build();
 
     // Save the image.
-    image.save("img/qrcode.png").unwrap();
+    image.save("img/qrcode3.png").unwrap();
 }
