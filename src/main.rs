fd24dcfd86d7b8dcb481c1d6ae7f8fa9a4368e7a
xTeKc <bcd3v@pm.me>
2021-10-14 20:34:03 -0400

    uncomment

--- src/main.rs
+++ src/main.rs
@@ -10,24 +10,25 @@ use colored::*;
 //     println!("Number Of Bytes Read: {}", b1);
 // }
 
-// fn qr_data() {
-//     let code = QrCode::new(b"H3LL0").unwrap();
-//     let string = code.render::<char>().quiet_zone(false).module_dimensions(2, 1).build();
-//     println!("{}", string);
-// }
-
-fn qr_save() {
-    // Encode some data into bits.
-    let code = QrCode::new(b"HELLO").unwrap();
-
-    // Render the bits into an image.
-    let image = code.render::<Luma<u8>>().build();
-
-    // Save the image.
-    image.save("./QR/QRcode.png").unwrap();
+fn qr_data() {
+    let code = QrCode::new(b"H3LL0").unwrap();
+    let string = code.render::<char>().quiet_zone(false).module_dimensions(2, 1).build();
+    println!("{}", string);
 }
 
+// fn qr_save() {
+//     // Encode some data into bits.
+//     let code = QrCode::new(b"HELLO").unwrap();
+
+//     // Render the bits into an image.
+//     let image = code.render::<Luma<u8>>().build();
+
+//     // Save the image.
+//     image.save("./QR/QRcode.png").unwrap();
+// }
+
 fn main() {
-    // qr_data();
+    // output();
+    qr_data();
     // qr_save();
 }
