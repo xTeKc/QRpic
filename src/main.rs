5e023bfb162d48c10b2990f44ba8d252d989a33a
xTeKc <bcd3v@pm.me>
2021-10-14 20:28:38 -0400

    add new imports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![allow(unused_must_use)]
+use qrcode::bits::Bits;
+use qrcode::types::Version;
 use qrcode::QrCode;
 use image::Luma;
 use colored::*;
@@ -10,24 +13,24 @@ use colored::*;
 //     println!("Number Of Bytes Read: {}", b1);
 // }
 
-fn qr_data() {
-    let code = QrCode::new(b"H3LL0").unwrap();
-    let string = code.render::<char>().quiet_zone(false).module_dimensions(2, 1).build();
-    println!("{}", string);
-}
-
-// fn qr_save() {
-//     // Encode some data into bits.
-//     let code = QrCode::new(b"01234567").unwrap();
-
-//     // Render the bits into an image.
-//     let image = code.render::<Luma<u8>>().build();
-
-//     // Save the image.
-//     image.save("./QR/qrcode.png").unwrap();
+// fn qr_data() {
+//     let code = QrCode::new(b"H3LL0").unwrap();
+//     let string = code.render::<char>().quiet_zone(false).module_dimensions(2, 1).build();
+//     println!("{}", string);
 // }
 
+fn qr_save() {
+    // Encode some data into bits.
+    let code = QrCode::new(b"HELLO").unwrap();
+
+    // Render the bits into an image.
+    let image = code.render::<Luma<u8>>().build();
+
+    // Save the image.
+    image.save("./QR/QRcode.png").unwrap();
+}
+
 fn main() {
-    qr_data();
-    // qr_save();
+    // qr_data();
+    qr_save();
 }
