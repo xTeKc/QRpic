518387340bc5c393f869750d45fe185ec4ff2292
xTeKc <bcd3v@pm.me>
2021-10-14 21:06:34 -0400

    generate qr randomly

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,26 @@ use rand::prelude::*;
 //     println!("Number Of Bytes Read: {}", b1);
 // }
 
-fn qr_data() {
-    let code = QrCode::new(b"H3LL0").unwrap();
-    let string = code.render::<char>().quiet_zone(false).module_dimensions(2, 1).build();
-    println!("{}", string);
+// fn qr_data() {
+//     let code = QrCode::new(b"H3LL0").unwrap();
+//     let string = code.render::<char>().quiet_zone(false).module_dimensions(2, 1).build();
+//     println!("{}", string);
+// }
+
+fn rand() {
+    let qr_rand: u8 = random();
+    let qr_output = QrCode::new(b" ").unwrap();
+    let ren = qr_output.render::<char>().quiet_zone(false).module_dimensions(2, 1).build();
+    println!("{}", qr_rand);
+
+    if random() { 
+        println!("{}",ren);
+    }
 }
 
-fn gui() {
-    let new_qr: String = " "; 
-
-
-}
+// fn gui() {
+    
+// }
 
 
 // fn qr_save() {
@@ -36,6 +45,7 @@ fn gui() {
 
 fn main() {
     // output();
-    qr_data();
+    // qr_data();
     // qr_save();
+    rand();
 }
