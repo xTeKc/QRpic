eee3bae0d9fa8a8d131656e46069f2b5f1f5926e
xTeKc <bcd3v@pm.me>
2021-12-26 23:19:35 -0500

    update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
 use qrcode::QrCode;
 
+// create a function that takes a string and returns a qrcode image as a png file with the string as the content
+fn create_qr_image(content: String) -> Result<image::ImageBuffer<image::Rgb<u8>, Vec<u8>>, qrcode::QrCodeError> {
+    let code = QrCode::new(content)?;
+    let mut image = code.to_image(10)?;
+    image.save("qr.png")?;
+    Ok(image)
+}
+
+// create a function that takes a string and returns a qrcode image as a png file with the string as the content
+fn create_qr_image_from_path(path: String) -> Result<image::ImageBuffer<image::Rgb<u8>, Vec<u8>>, qrcode::QrCodeError> {
+    let code = QrCode::new(path)?;
+    let mut image = code.to_image(10)?;
+    image.save("qr.png")?;
+    Ok(image)
+}
+
+// create a function that takes a string and returns a qrcode image as a png file with the string as the content
+fn create_qr_image_from_file(path: &str) -> Result<image::ImageBuffer<image::Rgb<u8>, Vec<u8>>, qrcode::QrCodeError> {
+    let code = QrCode::new(path)?;
+    let mut image = code.to_image(10)?;
+    image.save("qr.png")?;
+    Ok(image)
+}
+
 fn main() {
-    
+    let path = "qr.png";
+    let content = ""
+        .to_string();
 }
